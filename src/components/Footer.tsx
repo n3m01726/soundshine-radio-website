fcf7a5ca1222384891b0c44ad59217d92162a263
n3m01726 <118871349+n3m01726@users.noreply.github.com>
2025-03-03 01:32:21 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,27 +5,36 @@ import { FaDiscord } from "react-icons/fa"
 const Footer = () => {
   return (
     <div className="flex items-center space-x-4 mr-4">
-      <a 
-        href="https://www.facebook.com/soundshineradiocom/" 
-        target="_blank"
-        className="text-neutral-400 hover:text-white transition-colors"
-      >
-        <Facebook size={20} />
-      </a>
-      <a 
-        href="https://www.instagram.com/soundshineradio/" 
-        target="_blank"
-        className="text-neutral-400 hover:text-white transition-colors"
-      >
-        <Instagram size={20} />
-      </a>
-      <a 
+            <a 
         href="https://discord.gg/uhc7RUSk84" 
         target="_blank"
         className="text-neutral-400 hover:text-white transition-colors"
       >
         <FaDiscord size={20} />
       </a>
+      <a 
+        href="https://www.facebook.com/soundshiner/" 
+        target="_blank"
+        className="text-neutral-400 hover:text-white transition-colors"
+      >
+        <Facebook size={20} />
+      </a>
+      <a 
+        href="https://www.instagram.com/soundshiner/" 
+        target="_blank"
+        className="text-neutral-400 hover:text-white transition-colors"
+      >
+        <Instagram size={20} />
+      </a>
+
+      <a 
+        href="https://www.x.com/soundshiner/" 
+        target="_blank"
+        className="text-neutral-400 hover:text-white transition-colors"
+      >
+        <Twitter size={20} />
+      </a>
+      
     </div>
   )
 }
